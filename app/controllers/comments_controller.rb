a3e662651dcc825184230e618bec944d20e3de36
kazukioyama <kazuki.oy03@gmail.com>
2020-09-18 08:38:04 +0900

    コメント機能追加(途中から)

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,4 @@
-pclass CommentsController < ApplicationController
+class CommentsController < ApplicationController
   before_action :set_comment, only: [:show, :edit, :update, :destroy]
   before_action :authenticate_user!
 
@@ -32,6 +32,7 @@ pclass CommentsController < ApplicationController
       redirect_to [@post]
     else
       render 'posts/show'
+    end
   end
 
   # PATCH/PUT /comments/1
@@ -51,11 +52,9 @@ pclass CommentsController < ApplicationController
   # DELETE /comments/1
   # DELETE /comments/1.json
   def destroy
+    @comment = current_user.comments.find(params[:id])
     @comment.destroy
-    respond_to do |format|
-      format.html { redirect_to comments_url, notice: 'Comment was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    redirect_to [:post, { id: params[:post_id] }]
   end
 
   private
